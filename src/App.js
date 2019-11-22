4d6931e79115b471a8cb591b2b6cbfd3cdfae056
tianyu <tianyu5177@sina.com>
2019-11-22 17:08:30 +0800

    更新任务表

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import {Route,Switch} from 'react-router-dom'
-import Login from './pages/login/login'
+import Login from './pages/login/login' //引入的不是我们定义的那个Login，而是根据Login生成的一个新组件
 import Admin from './pages/admin/admin'
 
 

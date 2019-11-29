84645bc6dabb15140f34522f75bc8ef401535c50
tianyu <tianyu5177@sina.com>
2019-11-29 18:53:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component{
         <Switch>
           <Route path="/login" component={Login}/>
           <Route path="/admin" component={Admin}/>
-          <Redirect to="/admin"/>
+          <Redirect to="/admin/home"/>
         </Switch>
       </div>
     )
